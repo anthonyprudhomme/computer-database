bdb23114565d6febfa573436f824b3f3239e6b8b
anthonyprudhomme <anthony.prudhomme@epfedu.fr>
2018-05-22 15:40:35 +0200

    Implemented .properties reading to avoid leaking password. .properties file added to the .gitignore

--- maven/computer-database/src/main/java/org/excilys/computer_database/persistence/JdbcConnection.java
+++ maven/computer-database/src/main/java/org/excilys/computer_database/persistence/JdbcConnection.java
@@ -1,7 +1,11 @@
 package org.excilys.computer_database.persistence;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,10 +17,12 @@ public class JdbcConnection {
   private Connection connection;
   private static final Logger LOGGER = LoggerFactory.getLogger(JdbcConnection.class);
 
-  private static String url = "jdbc:mysql://localhost/computer-database-db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
+  private static String defaultUrl;
+  private static String usedUrl;
+  private static String testUrl;
 
-  private static String username = "admincdb";
-  private static String password = "qwerty1234";
+  private static String username;
+  private static String password;
 
   public static boolean testMode = false;
 
@@ -24,16 +30,14 @@ public class JdbcConnection {
    * Set configuration parameters to test mode.
    */
   private static void setTestConfiguration() {
-    url = "jdbc:hsqldb:mem:computerdatabase";
+    usedUrl = testUrl;
   }
 
   /**
    * Set configuration parameters to default.
    */
   private static void setDefaultConfiguration() {
-    url = "jdbc:mysql://localhost/computer-database-db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
-    username = "admincdb";
-    password = "qwerty1234";
+    usedUrl = defaultUrl;
   }
 
   /**
@@ -43,7 +47,7 @@ public class JdbcConnection {
    */
   private static Connection getTestConnection() {
     try {
-      return DriverManager.getConnection(url);
+      return DriverManager.getConnection(usedUrl);
     } catch (SQLException e) {
       e.printStackTrace();
       return null;
@@ -55,6 +59,7 @@ public class JdbcConnection {
    * Singleton. Used to do the connection to the database.
    */
   private JdbcConnection() {
+    readPropertyFile();
     if (testMode) {
       setTestConfiguration();
       connection = getTestConnection();
@@ -63,10 +68,37 @@ public class JdbcConnection {
       System.out.println("Connecting database...");
 
       try {
-        connection = DriverManager.getConnection(url, username, password);
+        connection = DriverManager.getConnection(usedUrl, username, password);
         System.out.println("Database connected!");
       } catch (SQLException e) {
         throw new IllegalStateException("Cannot connect the database!", e);
+      }
+    }
+  }
+
+  /**
+   * Read the .properties file to get credentials.
+   */
+  private void readPropertyFile() {
+    final Properties prop = new Properties();
+    InputStream input = null;
+    try {
+      input = new FileInputStream("config.properties");
+      prop.load(input);
+      defaultUrl = prop.getProperty("default.url");
+      username = prop.getProperty("default.username");
+      password = prop.getProperty("default.password");
+      testUrl = prop.getProperty("test.url");
+
+    } catch (final IOException ex) {
+      ex.printStackTrace();
+    } finally {
+      if (input != null) {
+        try {
+          input.close();
+        } catch (final IOException e) {
+          e.printStackTrace();
+        }
       }
     }
   }
@@ -87,7 +119,7 @@ public class JdbcConnection {
         } else {
           setDefaultConfiguration();
           if (instance.connection.isClosed()) {
-            instance.connection = DriverManager.getConnection(url, username, password);
+            instance.connection = DriverManager.getConnection(usedUrl, username, password);
           }
         }
       }
