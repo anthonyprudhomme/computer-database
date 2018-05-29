6799c9c7d16b4156c065dbd3cd6ecc091303fb11
anthonyprudhomme <anthony.prudhomme@epfedu.fr>
2018-05-29 10:56:26 +0200

    Selenium tests

--- maven/computer-database/src/main/java/org/excilys/computer_database/servlets/AddComputerServlet.java
+++ maven/computer-database/src/main/java/org/excilys/computer_database/servlets/AddComputerServlet.java
@@ -28,7 +28,8 @@ public class AddComputerServlet extends HttpServlet {
   public static final String COMPANY_ID = "companyId";
 
   public static final String ADD_COMPUTER = "/WEB-INF/views/addComputer.jsp";
-  public static final String DASHBOARD = "/WEB-INF/views/dashboard.jsp";
+  //public static final String DASHBOARD = "/WEB-INF/views/dashboard.jsp";
+  public static final String DASHBOARD = "list-computer";
 
   /**
    * Overload of doGet method.
@@ -43,8 +44,9 @@ public class AddComputerServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     String computerName = request.getParameter(COMPUTER_NAME);
-    Date introduced = Date.valueOf(request.getParameter(INTRODUCED));
-    Date discontinued = Date.valueOf(request.getParameter(DISCONTINUED));
+    Date introduced = getDateFromInput(request, INTRODUCED);
+    Date discontinued = getDateFromInput(request, DISCONTINUED);
+
     int companyId = Integer.valueOf(request.getParameter(COMPANY_ID));
     Company company = CompanyService.getInstance().getCompany(companyId);
 
@@ -54,12 +56,29 @@ public class AddComputerServlet extends HttpServlet {
     if (result.left == ComputerValidationStatus.OK) {
       ArrayList<Computer> computers = ComputerService.getInstance().getComputers();
       request.setAttribute("computers", computers);
-      this.getServletContext().getRequestDispatcher(DASHBOARD).forward(request, response);
+      response.sendRedirect(DASHBOARD);
+
+      //this.getServletContext().getRequestDispatcher(DASHBOARD).forward(request, response);
     } else {
       ArrayList<Company> companies = CompanyService.getInstance().getCompanies();
       request.setAttribute("companies", companies);
       request.setAttribute("error", result.right);
       this.getServletContext().getRequestDispatcher(ADD_COMPUTER).forward(request, response);
     }
+  }
+
+  /**
+   * Return the date from the user input. Returns null if nothing was entered.
+   * @param request HttpServletRequest to get the param
+   * @param paramName name of the param you want to check in the request
+   * @return the date.
+   */
+  public Date getDateFromInput(HttpServletRequest request, String paramName) {
+    Date date = null;
+    String userInput = request.getParameter(paramName);
+    if (!userInput.isEmpty()) {
+      date = Date.valueOf(request.getParameter(paramName));
+    }
+    return date;
   }
 }
